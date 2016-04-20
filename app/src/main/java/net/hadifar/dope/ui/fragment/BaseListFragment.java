13bf4204a63a203feb0a7164ef90c036ff30754b
Amir Hadifar <hadifar.amir@gmail.com>
2016-04-21 00:40:07 +0430

    FabBehavior added.

--- app/src/main/java/net/hadifar/dope/ui/fragment/BaseListFragment.java
+++ app/src/main/java/net/hadifar/dope/ui/fragment/BaseListFragment.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 
 import net.hadifar.dope.R;
 import net.hadifar.dope.ui.adapter.BaseAdapter;
@@ -34,6 +35,7 @@ public abstract class BaseListFragment extends BaseFragment {
         View view = inflater.inflate(R.layout.fragment_base_list, container, false);
         ButterKnife.bind(this, view);
 
+
         return view;
     }
 
@@ -43,9 +45,29 @@ public abstract class BaseListFragment extends BaseFragment {
             public void run() {
                 if (!fab.isShown()) {
                     fab.show();
+                    setupFabListener();
                 }
             }
         }, 1000);
+    }
+
+    private void setupFabListener() {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                if (dy > 0 || dy < 0 && fab.isShown())
+                    fab.hide();
+            }
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE) {
+                    fab.show();
+                }
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+        });
     }
 
 }
